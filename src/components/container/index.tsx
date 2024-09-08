3292ba21440fd9d609d400f4fe85f924c7db21ec
sem-a <aleksey.i99@mail.ru>
2024-09-08 16:21:51 +0300

    git init

---
+++ src/components/container/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from "./index.module.css";
+
+type Props = {
+    children: React.ReactNode
+}
+
+export const Container: React.FC<Props> = ({children}) => {
+  return (
+    <div className={styles.container}>{children}</div>
+  )
+}
